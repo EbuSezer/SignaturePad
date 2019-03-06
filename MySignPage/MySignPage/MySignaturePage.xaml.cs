feedc188ba603bf0ceefab1733d8f7cda3226a71
ebusezer <ebubekirssezer@gmail.com>
2019-03-06 23:17:17 +0300

    Initially Created

---
+++ MySignPage/MySignPage/MySignaturePage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using SignaturePad.Forms;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MySignPage
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MySignaturePage : ContentPage
+	{
+		public MySignaturePage ()
+		{
+			InitializeComponent ();
+		}
+
+        private void ClearButton_Clicked(object sender, EventArgs e)
+        {
+            mySignView.Clear();
+        }
+
+        private async void SaveButton_Clicked(object sender, EventArgs e)
+        {
+            Stream image = await mySignView.GetImageStreamAsync(SignatureImageFormat.Jpeg);
+        }
+    }
+}
